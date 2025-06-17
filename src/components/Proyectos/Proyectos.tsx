c2f5cf7d217a5d0eb25f1f4f642d1d13a4529fa8
MrLuXuS <gggggsaucedo@gmail.com>
2025-06-17 11:09:49 -0400

    rebuilding

--- src/components/Proyectos/Proyectos.tsx
+++ src/components/Proyectos/Proyectos.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import './Proyectos.css';
 import Select from 'react-select';
